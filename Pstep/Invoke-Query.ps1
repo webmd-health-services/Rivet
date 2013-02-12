d938cb703d74c3c3c0dcc482b90b7cc7e69b5622
Aaron Jensen <ajensen@webmd.net>
2013-02-12 00:00:02 -0800

    Improving documentation.

--- Pstep/Invoke-Query.ps1
+++ Pstep/Invoke-Query.ps1
@@ -1,6 +1,23 @@
 
 function Invoke-Query
 {
+    <#
+    .SYNOPSIS
+    Executes a SQL query against the database.
+    
+    .DESCRIPTION
+    All migrations eventually come down to this method.  It takes raw SQL and executes it against the database.
+    
+    .EXAMPLE
+    Invoke-Query -Query 'create table pstep.Migrations( )'
+    
+    Executes the create table syntax above against the database.
+    
+    .EXAMPLE
+    Invoke-Query -Query 'select count(*) from MyTable' -Database MyOtherDatabase
+    
+    Executes a query against the non-current database.  Returns the rows as objects.
+    #>
     [CmdletBinding()]
     param(
         $Query,
