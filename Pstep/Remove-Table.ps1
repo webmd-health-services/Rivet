9793751264df7eb4e1b80674f2700b2f3af5eac4
saraujo <saraujo@webmd.com>
2013-06-26 15:48:57 -0700

    Adding Remove-Table.

---
+++ Pstep/Remove-Table.ps1
@@ -0,0 +1,31 @@
+
+function Remove-Table
+{
+    <#
+    .SYNOPSIS
+    Removes a table from a database.
+
+    .DESCRIPTION
+    You can't get any of the data back, so be careful.
+
+    .EXAMPLE
+    Remove-Table -Name 'Coffee'
+
+    Removes the `Coffee` table from the database.
+    #>
+    param(
+        # The name of the table where the column should be removed.
+        [Parameter(Mandatory=$true)]
+        [string]
+        $Name,
+
+        [Alias('TableSchema')]
+        [string]
+        # The schema of the table where the column should be added.  Default is `dbo`.
+        $SchemaName = 'dbo'
+    )
+
+    Write-Host (' -{0}.{1}' -f $SchemaName,$Name)
+    $query = 'drop table [{0}].[{1}]' -f $SchemaName,$Name
+    Invoke-Query $query
+}
