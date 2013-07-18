180e85cde8990f376dbe9a927eac977dc863704a
Phil Tseng <ptseng@users.noreply.github.com>
2013-07-17 17:24:01 -0700

    Added tests for PRIMARY KEY: multiple columns, noncluster, indexoptions.  WORK IN PROGRESS

--- Test/Test-AddPrimaryKey.ps1
+++ Test/Test-AddPrimaryKey.ps1
@@ -20,18 +20,42 @@ function Test-ShouldAddPrimaryKey
 
 function Test-ShouldAddPrimaryKeyWithMultipleColumns
 {
+    Invoke-Rivet -Push 'AddTableWithPrimaryKeyWithMultipleColumns'
+    Assert-True (Test-Table 'PrimaryKey')
+
+    Assert-PrimaryKey -TableName 'PrimaryKey' -ColumnName 'id','uuid','date'
+
 }
 
 function Test-ShouldAddNonClusteredPrimaryKey
 {
+    Invoke-Rivet -Push 'AddNonClusteredPrimaryKey'
+    Assert-True (Test-Table 'PrimaryKey')
+
+    Assert-PrimaryKey -TableName 'PrimaryKey' -ColumnName 'id' -NonClustered
 }
 
 function Test-ShouldSetIndexOptions
 {
+    Invoke-Rivet -Push 'SetIndexOptions'
+    Assert-True (Test-Table 'PrimaryKey')
+
+    Assert-PrimaryKey -TableName 'PrimaryKey' -ColumnName 'id' -WithOptions
 }
 
 function Test-ShouldRemovePrimaryKey
 {
+    # Add Primary Key, Assert
+
+    Invoke-Rivet -Push 'AddTableWithPrimaryKey'
+    Assert-True (Test-Table 'PrimaryKey')
+    Assert-PrimaryKey -TableName 'PrimaryKey' -ColumnName 'id'
+
+    # Remove Primary Key, Assert
+
+    Invoke-Rivet -Pop 
+
+
 }
 
 function Test-ShouldAddPrimaryKeyToTableInCustomSchema
