14ff8669808e27d18a3697010455d7fe7a31502c
Phil Tseng <ptseng@webmd.net>
2013-08-22 15:34:53 -0700

    Create object for Remove-Column operation

---
+++ Source/Test/Operations/RemoveColumnOperationTestFixture.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using Rivet.Operations;
+
+namespace Rivet.Test.Operations
+{
+	[TestFixture]
+	public sealed class RemoveColumnOperationTestFixture
+	{
+		[SetUp]
+		public void SetUp()
+		{
+		}
+
+		[Test]
+		public void ShouldSetPropertiesforColumn()
+		{
+			var tableName = "tableName";
+			var schemaName = "schemaName";
+			var columnName = "columnName";
+
+			var op = new RemoveColumnOperation(schemaName, tableName, columnName);
+			Assert.That(op.TableName, Is.EqualTo(tableName));
+			Assert.That(op.SchemaName, Is.EqualTo(schemaName));
+			Assert.That(op.ColumnName, Is.SameAs(columnName));
+
+			var expectedQuery = string.Format("alter table [{0}].[{1}] drop column [{2}]", schemaName, tableName, columnName);
+			Assert.That(op.ToQuery(), Is.EqualTo(expectedQuery));
+
+		}
+	}
+}
