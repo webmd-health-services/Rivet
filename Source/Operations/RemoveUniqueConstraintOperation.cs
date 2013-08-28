aa4324ab0027f7638453b0b6aca511587e571056
Phil Tseng <ptseng@users.noreply.github.com>
2013-08-28 11:35:12 -0700

    Create object for Remove-UniqueConstraint operation

--- Source/Operations/RemoveUniqueConstraintOperation.cs
+++ Source/Operations/RemoveUniqueConstraintOperation.cs
@@ -4,14 +4,22 @@ namespace Rivet.Operations
 {
 	public sealed class RemoveUniqueConstraintOperation : Operation
 	{
-		public RemoveUniqueConstraintOperation(string schemaName, string tableName, string columnName)
+		public RemoveUniqueConstraintOperation(string schemaName, string tableName, string[] columnName)
 		{
-			
+			Cons = new ConstraintName(schemaName, tableName, columnName, ConstraintType.Unique);
+			SchemaName = schemaName;
+			TableName = tableName;
+			ColumnName = (string[])columnName.Clone(); //Used only for testing
 		}
+
+		public ConstraintName Cons { get; private set; }
+		public string SchemaName { get; private set; }
+		public string TableName { get; private set; }
+		public string[] ColumnName { get; private set; }
 
 		public override string ToQuery()
 		{
-			throw new NotImplementedException();
+			return string.Format("alter table {0}.{1} drop constraint {2}", SchemaName, TableName, Cons.ReturnConstraintName());
 		}
 	}
 }
