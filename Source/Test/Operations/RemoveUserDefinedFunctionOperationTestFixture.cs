f63850c4fc0188088a2e2ae61cde890bfa181f0c
Phil Tseng <ptseng@webmd.net>
2013-09-03 10:16:51 -0700

    Add renamed files, in concert with last commit

---
+++ Source/Test/Operations/RemoveUserDefinedFunctionOperationTestFixture.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using Rivet.Operations;
+
+namespace Rivet.Test.Operations
+{
+	[TestFixture]
+	public sealed class RemoveUserDefinedFunctionOperationTestFixture
+	{
+		const string SchemaName = "schemaName";
+		const string ProcedureName = "procedureName";
+
+		[SetUp]
+		public void SetUp()
+		{
+
+		}
+
+		[Test]
+		public void ShouldSetPropertiesForRemoveUserDefinedFunction()
+		{
+			var op = new RemoveUserDefinedFunctionOperation(SchemaName, ProcedureName);
+			Assert.AreEqual(SchemaName, op.SchemaName);
+			Assert.AreEqual(ProcedureName, op.ProcedureName);
+		}
+
+		[Test]
+		public void ShouldWriteQueryForRemoveUserDefinedFunction()
+		{
+			var op = new RemoveUserDefinedFunctionOperation(SchemaName, ProcedureName);
+			const string expectedQuery = "drop function [schemaName].[procedureName]";
+			Assert.AreEqual(expectedQuery, op.ToQuery());
+		}
+	}
+}
