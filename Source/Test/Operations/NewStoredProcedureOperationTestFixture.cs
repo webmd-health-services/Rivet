fd52309f6f038d98c35c20016cc5e26dee1bbaff
Phil Tseng <ptseng@webmd.net>
2013-09-03 13:19:39 -0700

    Create New-StoredProcedure migration operation

---
+++ Source/Test/Operations/NewStoredProcedureOperationTestFixture.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using Rivet.Operations;
+
+namespace Rivet.Test.Operations
+{
+	[TestFixture]
+	public sealed class NewStoredProcedureOperationTestFixture
+	{
+		const string SchemaName = "schemaName";
+		const string ProcedureName = "procedureName";
+		const string Definition = "definition";
+
+		[SetUp]
+		public void SetUp()
+		{
+
+		}
+
+		[Test]
+		public void ShouldSetPropertiesForNewStoredProcedure()
+		{
+			var op = new NewStoredProcedureOperation(SchemaName, ProcedureName, Definition);
+			Assert.AreEqual(SchemaName, op.SchemaName);
+			Assert.AreEqual(ProcedureName, op.Name);
+			Assert.AreEqual(Definition, op.Definition);
+		}
+
+		[Test]
+		public void ShouldWriteQueryForNewStoredProcedure()
+		{
+			var op = new NewStoredProcedureOperation(SchemaName, ProcedureName, Definition);
+			const string expectedQuery = "create procedure [schemaName].[procedureName] as definition";
+			Assert.AreEqual(expectedQuery, op.ToQuery());
+		}
+
+	}
+}
