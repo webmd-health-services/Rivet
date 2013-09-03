6bd2cf6067bd6f445facf5a85fb5deafd996e8c6
Phil Tseng <ptseng@users.noreply.github.com>
2013-09-03 15:24:37 -0700

    Create New-View migration operation

---
+++ Source/Operations/NewViewOperation.cs
@@ -0,0 +1,21 @@
+﻿namespace Rivet.Operations
+{
+	public sealed class NewViewOperation : Operation
+	{
+		public NewViewOperation(string schemaName, string name, string definition)
+		{
+			SchemaName = schemaName;
+			Name = name;
+			Definition = definition;
+		}
+
+		public string SchemaName { get; private set; }
+		public string Name { get; private set; }
+		public string Definition { get; private set; }
+
+		public override string ToQuery()
+		{
+			return string.Format(@"create view [{0}].[{1}] {2}", SchemaName, Name, Definition);
+		}
+	}
+}
