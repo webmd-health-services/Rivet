2e0e749d5166c624502889ea6dec1e41e3193beb
Phil Tseng <ptseng@webmd.net>
2013-09-17 10:13:31 -0700

    Create Add-Trigger and Remove-Trigger functions

---
+++ Source/Operations/RemoveTriggerOperation.cs
@@ -0,0 +1,19 @@
+﻿namespace Rivet.Operations
+{
+	public sealed class RemoveTriggerOperation : Operation
+	{
+		public RemoveTriggerOperation(string schemaName, string triggerName)
+		{
+			SchemaName = schemaName;
+			TriggerName = triggerName;
+		}
+
+		public string SchemaName { get; private set; }
+		public string TriggerName { get; private set; }
+
+		public override string ToQuery()
+		{
+			return string.Format("drop trigger [{0}].[{1}]", SchemaName, TriggerName);
+		}
+	}
+}
