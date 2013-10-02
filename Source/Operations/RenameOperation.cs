7b0b96c1bbde208ddc19226b31904f738532628e
Phil Tseng <ptseng@webmd.net>
2013-10-02 15:50:19 -0700

    Added Rename-Table Function

---
+++ Source/Operations/RenameOperation.cs
@@ -0,0 +1,39 @@
+﻿namespace Rivet.Operations
+{
+	public sealed class RenameOperation : Operation
+	{
+		private enum ObjectType
+		{
+			Table,
+			Column,
+			Database,
+			Index,
+			Object,
+			Statistics,
+			Userdatatype
+		}
+
+		public RenameOperation(string schemaName, string currentName, string newName)
+		{
+			SchemaName = schemaName;
+			CurrentName = currentName;
+			NewName = newName;
+			Type = ObjectType.Table;
+		}
+
+		public string SchemaName { get; private set; }
+		public string CurrentName { get; private set; }
+		public string NewName { get; private set; }
+		private ObjectType Type { get; set; }
+
+		public override string ToQuery()
+		{
+			if (Type == ObjectType.Table)
+			{
+				return string.Format("declare @valback int; exec @valback = sp_rename '{0}.{1}', '{2}'; select @valback;", SchemaName, CurrentName, NewName);
+			}
+
+			return "";
+		}
+	}
+}
---
+++ Source/Test/Operations/RenameOperationTestFixture.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using Rivet.Operations;
+
+namespace Rivet.Test.Operations
+{
+	[TestFixture]
+	public sealed class RenameOperationTestFixture
+	{
+		const string SchemaName = "schemaName";
+		const string CurrentName = "currentName";
+		const string NewName = "newName";
+
+		[SetUp]
+		public void SetUp()
+		{
+
+		}
+
+		[Test]
+		public void ShouldSetPropertiesForRenameOperation()
+		{
+			var op = new RenameOperation(SchemaName, CurrentName, NewName);
+			Assert.AreEqual(SchemaName, op.SchemaName);
+			Assert.AreEqual(CurrentName, op.CurrentName);
+			Assert.AreEqual(NewName, op.NewName);
+		}
+
+		[Test]
+		public void ShouldWriteQueryForAddViewOperation()
+		{
+			var op = new RenameOperation(SchemaName, CurrentName, NewName);
+			const string expectedQuery = "declare @valback int; exec @valback = sp_rename 'schemaName.currentName', 'newName'; select @valback;";
+			Assert.AreEqual(expectedQuery, op.ToQuery());
+		}
+
+	}
+}
