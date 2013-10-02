3464d015aa057724369f37b34e2b434c9228a8cf
Phil Tseng <ptseng@webmd.net>
2013-10-02 09:51:32 -0700

    Created function for update-trigger

---
+++ Source/Operations/UpdateTriggerOperation.cs
@@ -0,0 +1,21 @@
+﻿namespace Rivet.Operations
+{
+	public sealed class UpdateTriggerOperation : Operation
+	{
+		public UpdateTriggerOperation(string schemaName, string name, string definition)
+		{
+			SchemaName = schemaName;
+			Name = name;
+			Definition = definition;
+		}
+
+		public string SchemaName { get; private set; }
+		public string Name { get; private set; }
+		public string Definition { get; private set; }
+
+		public override string ToQuery()
+		{
+			return string.Format(@"alter trigger [{0}].[{1}] {2}", SchemaName, Name, Definition);
+		}
+	}
+}
