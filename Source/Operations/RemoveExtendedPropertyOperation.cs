5cd252878cd03514c0eae6d5740d01ed63c1d007
Phil Tseng <ptseng@webmd.net>
2013-10-09 13:01:03 -0700

    Create Remove-ExtendedProperty Rivet function, refactor Remove-Description to use this new function

--- Source/Operations/RemoveExtendedPropertyOperation.cs
+++ Source/Operations/RemoveExtendedPropertyOperation.cs
@@ -4,37 +4,53 @@ namespace Rivet.Operations
 {
 	public sealed class RemoveExtendedPropertyOperation : Operation
 	{
-		public RemoveExtendedPropertyOperation(string schemaName, string tableName, string columnName)
+		// Schema
+		public RemoveExtendedPropertyOperation(string schemaName, string name)
+		{
+			ForSchema = true;
+			SchemaName = schemaName;
+			Name = name;
+		}
+
+		// Table
+		public RemoveExtendedPropertyOperation(string schemaName, string tableName, string name)
+		{
+			ForTable = true;
+			SchemaName = schemaName;
+			TableName = tableName;
+			Name = name;
+		}
+
+		// Column
+		public RemoveExtendedPropertyOperation(string schemaName, string tableName, string columnName, string name)
 		{
 			ForColumn = true;
 			SchemaName = schemaName;
 			TableName = tableName;
 			ColumnName = columnName;
+			Name = name;
 		}
 
-		public RemoveExtendedPropertyOperation(string schemaName, string tableName)
-		{
-			ForTable = true;
-			SchemaName = schemaName;
-			TableName = tableName;
-		}
-
-		public bool ForColumn { get; private set; }
+		public bool ForSchema { get; private set; }
 		public bool ForTable { get; private set; }
+		public bool ForColumn { get; private set; }
 		public string SchemaName { get; private set; }
 		public string TableName { get; private set; }
 		public string ColumnName { get; private set; }
+		public string Name { get; private set; }
 
 		public override string ToQuery()
 		{
-			var query = string.Format(@"
-			EXEC sys.sp_dropextendedproperty	@name=N'MS_Description',
-												@level0type=N'SCHEMA', @level0name='{0}',
-												@level1type=N'TABLE',  @level1name='{1}'", SchemaName, TableName);
+			var query = string.Format(@"EXEC sys.sp_dropextendedproperty{2}@name=N'{0}',{2}@level0type=N'SCHEMA', @level0name=N'{1}'", Name, SchemaName, Environment.NewLine);
 
-			if (ForTable == false && ForColumn == true && !string.IsNullOrEmpty(ColumnName))
+			if (ForTable || ForColumn)
 			{
-				query += string.Format(",\n												@level2type=N'COLUMN', @level2name='{0}'", ColumnName);
+				query += string.Format(",{1}@level1type=N'TABLE', @level1name='{0}'", TableName, Environment.NewLine);
+			}
+
+			if (ForColumn)
+			{
+				query += string.Format(",{1}@level2type=N'COLUMN', @level2name='{0}'", ColumnName, Environment.NewLine);
 			}
 
 			return query;
---
+++ Test/Test-RemoveExtendedProperty.ps1
@@ -0,0 +1,92 @@
+function Setup
+{
+    Import-Module -Name (Join-Path $TestDir 'RivetTest') -ArgumentList 'RemoveExtendedProperty' 
+    Start-RivetTest
+}
+
+function TearDown
+{
+    Stop-RivetTest
+    Remove-Module RivetTest
+}
+
+function Test-ShouldRemoveExtendedPropertyToSchema
+{
+    @'
+function Push-Migration
+{
+    Invoke-Query 'create schema fizz'
+    Add-ExtendedProperty -Name 'Deploy' -Value 'TRUE' -SchemaName 'fizz'
+    Remove-ExtendedProperty -Name 'Deploy' -SchemaName 'fizz'
+}
+
+function Pop-Migration
+{
+    
+}
+
+'@ | New-Migration -Name 'RemoveExtendedPropertyToSchema'
+
+    Invoke-Rivet -Push 'RemoveExtendedPropertyToSchema'
+
+    $expinfo = Get-ExtendedProperties
+
+    Assert-Null $expinfo
+
+}
+
+function Test-ShouldRemoveExtendedPropertyToTable
+{
+    @'
+function Push-Migration
+{
+    Add-Table Foobar {
+        Int ID
+    }
+
+    Add-ExtendedProperty -Name 'Deploy' -Value 'TRUE' -TableName 'Foobar' -SchemaName 'dbo'
+    Remove-ExtendedProperty -Name 'Deploy' -TableName 'Foobar' -SchemaName 'dbo'
+}
+
+function Pop-Migration
+{
+    
+}
+
+'@ | New-Migration -Name 'RemoveExtendedPropertyToTable'
+
+    Invoke-Rivet -Push 'RemoveExtendedPropertyToTable'
+
+    $expinfo = Get-ExtendedProperties
+
+    Assert-Null $expinfo
+
+}
+
+function Test-ShouldRemoveExtendedPropertyToColumn
+{
+    @'
+function Push-Migration
+{
+    Add-Table Foobar {
+        Int ID
+    }
+
+    Add-ExtendedProperty -Name 'Deploy' -Value 'TRUE' -TableName 'Foobar' -SchemaName 'dbo' -ColumnName 'ID'
+    Remove-ExtendedProperty -Name 'Deploy' -TableName 'Foobar' -SchemaName 'dbo' -ColumnName 'ID'
+}
+
+function Pop-Migration
+{
+    
+}
+
+'@ | New-Migration -Name 'RemoveExtendedPropertyToColumn'
+
+    Invoke-Rivet -Push 'RemoveExtendedPropertyToColumn'
+
+    $expinfo = Get-ExtendedProperties
+
+    Assert-Null $expinfo
+
+}
