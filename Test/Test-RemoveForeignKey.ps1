d989c0e73f1d9f2592146080e462975037565739
Aaron Jensen <splatteredbits@users.noreply.github.com>
2013-10-11 18:06:27 -0700

    Moving migration for foreign key functions out of external files into test fixtures.

--- Test/Test-RemoveForeignKey.ps1
+++ Test/Test-RemoveForeignKey.ps1
@@ -1,11 +1,11 @@
 
-function Setup
+function Start-Test
 {
-    Import-Module -Name (Join-Path $TestDir 'RivetTest') -ArgumentList 'RemoveForeignKey' 
+    Import-Module -Name (Join-Path $TestDir 'RivetTest') -ArgumentList 'RivetTest' 
     Start-RivetTest
 }
 
-function TearDown
+function Stop-Test
 {
     Stop-RivetTest
     Remove-Module RivetTest
@@ -13,6 +13,29 @@ function TearDown
 
 function Test-ShouldRemoveForeignKey
 {
+    @'
+function Push-Migration()
+{
+    Add-Table -Name 'Source' {
+        Int 'source_id' -NotNull
+    }
+
+    Add-Table -Name 'Reference' {
+        Int 'reference_id' -NotNull
+    }
+
+    Add-PrimaryKey -TableName 'Reference' -ColumnName 'reference_id'
+    Add-ForeignKey -TableName 'Source' -ColumnName 'source_id' -References 'Reference' -ReferencedColumn 'reference_id'
+}
+
+function Pop-Migration()
+{
+    Remove-ForeignKey -TableName 'Source' -References 'Reference'
+}
+'@ | New-Migration -Name 'RemoveForeignKey'
     Invoke-Rivet -Push "RemoveForeignKey"
+    Assert-ForeignKey -TableName 'Source' -References 'Reference'
+
+    Invoke-Rivet -Pop
     Assert-ForeignKey -TableName 'Source' -References 'Reference' -TestRemoval
 }
