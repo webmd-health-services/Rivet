eae2f92bf94a805c7da3e930dfbbdc7d26f56258
Aaron Jensen <splatteredbits@users.noreply.github.com>
2013-10-17 15:14:53 -0700

    Updating Add-DefaultConstraint to make TableName, ColumnName, and Expression paramters switches optional.

--- Rivet/Add-DefaultConstraint.ps1
+++ Rivet/Add-DefaultConstraint.ps1
@@ -26,7 +26,7 @@ function Add-DefaultConstraint
 
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory=$true,Position=0)]
         [string]
         # The name of the target table.
         $TableName,
@@ -36,15 +36,15 @@ function Add-DefaultConstraint
         # The schema name of the target table.  Defaults to `dbo`.
         $SchemaName = 'dbo',
 
-        [Parameter(Mandatory=$true)]
-        [string]
-        #The default expression
-        $Expression,
-
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory=$true,Position=1)]
         [string]
         # The column on which to add the default constraint
         $ColumnName,
+
+        [Parameter(Mandatory=$true,Position=2)]
+        [string]
+        #The default expression
+        $Expression,
 
         [Parameter()]
         [switch]
