82dc15c4449739581ac7f6a8055b77a7bebe3bc5
Aaron Jensen <splatteredbits@users.noreply.github.com>
2013-10-18 12:46:10 -0700

    Merging 0.1 release into default.

--- Test/Test-RemoveDescription.ps1
+++ Test/Test-RemoveDescription.ps1
@@ -1,11 +1,11 @@
 
-function Setup
+function Start-Test
 {
-    Import-Module -Name (Join-Path $TestDir 'RivetTest') -ArgumentList 'MS_Description' 
+    Import-Module -Name (Join-Path $TestDir 'RivetTest') -ArgumentList 'RivetTest' 
     Start-RivetTest
 }
 
-function TearDown
+function Stop-Test
 {
     Stop-RivetTest
     Remove-Module RivetTest
@@ -13,13 +13,60 @@ function TearDown
 
 function Test-ShouldUpdateTableAndColumnDescription
 {
+    @'
+function Push-Migration()
+{
+    Invoke-Query @"
+    create table [MS_Description] (
+        add_description varchar(max)
+    )
+"@
+
+    Add-Description -Description 'new description' -TableName 'MS_Description'
+    Add-Description -Description 'new description' -TableName 'MS_Description' -ColumnName 'add_description'
+}
+
+function Pop-Migration()
+{
+    Invoke-Query 'drop table [MS_Description]'
+}
+'@ | New-Migration -Name 'AddDescription'
+
     Invoke-Rivet -Push 'AddDescription'
     Assert-Table -Name 'MS_Description' -Description 'new description'
     Assert-Column -Name 'add_description' 'varchar' -Description 'new description' -TableName MS_Description
 
+    @'
+function Push-Migration()
+{
+    Update-Description -Description 'updated description' -TableName MS_Description
+    Update-Description -Description 'updated description' -TableName MS_Description -ColumnName 'add_description'
+}
+
+function Pop-Migration()
+{
+    Update-Description -Description 'new description' -TableName MS_Description
+    Update-Description -Description 'new description' -TableName MS_Description -ColumnName 'add_description'
+}
+'@ | New-Migration -Name 'UpdateDescription'
+
     Invoke-Rivet -Push 'UpdateDescription'
     Assert-Table -Name 'MS_Description' -Description 'updated description'
     Assert-Column -Name 'add_description' 'varchar' -Description 'updated description' -TableName MS_Description
+
+    @'
+function Push-Migration()
+{
+    Remove-Description -TableName MS_Description 
+    Remove-Description -TableName MS_Description -ColumnName 'add_description'
+}
+
+function Pop-Migration()
+{
+    Add-Description -Description 'updated description' -TableName MS_Description
+    Add-Description -Description 'updated description' -TableName MS_Description -ColumnName 'add_description'
+}
+'@ | New-Migration -Name 'RemoveDescription'
 
     Invoke-Rivet -Push 'RemoveDescription'
     Assert-Table -Name 'MS_Description' -Description $null 
