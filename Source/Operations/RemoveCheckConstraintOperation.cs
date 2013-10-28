d8fae6f9d3138a0cd9890d489b8f5d1855dbb8b8
Phil Tseng <ptseng@users.noreply.github.com>
2013-10-28 12:10:21 -0700

    Added functions for Add-CheckConstraint and Remove-CheckConstraint

---
+++ Source/Operations/RemoveCheckConstraintOperation.cs
@@ -0,0 +1,22 @@
+﻿namespace Rivet.Operations
+{
+	public sealed class RemoveCheckConstraintOperation : Operation
+	{
+		public RemoveCheckConstraintOperation(string schemaName, string tableName, string constraintName)
+		{
+
+			SchemaName = schemaName;
+			TableName = tableName;
+			ConstraintName = constraintName;
+		}
+
+		public string SchemaName { get; private set; }
+		public string TableName { get; private set; }
+		public string ConstraintName { get; private set; }
+
+		public override string ToQuery()
+		{
+			return string.Format("alter table [{0}].[{1}] drop constraint [{2}]", SchemaName, TableName, ConstraintName);
+		}
+	}
+}
