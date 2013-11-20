a300f3687a521cc7e7dd1011508bbe84e7729a2a
Aaron Jensen <ajensen@webmd.net>
2013-11-20 09:58:21 -0800

    Moving tests for RemoveIndexOperation into their own class.

--- Source/Test/Operations/AddRemoveIndexOperationTestFixture.cs
+++ Source/Test/Operations/AddRemoveIndexOperationTestFixture.cs
@@ -200,58 +200,6 @@ namespace Rivet.Test.Operations
 			Assert.AreEqual(expectedQuery, op.ToQuery());
 		}
 
-		[Test]
-		public void ShouldSetPropertiesForRemoveIndex()
-		{
-			const string schemaName = "schemaName";
-			const string tableName = "tableName";
-			var columnName = new[] { "column1", "column2" };
-			var smokeColumnName = new[] { "column1" };
-
-			var op = new RemoveIndexOperation(schemaName, tableName, columnName);
-			Assert.AreEqual(schemaName, op.SchemaName);
-			Assert.AreEqual(tableName, op.TableName);
-			Assert.AreEqual(columnName, op.ColumnName);
-			Assert.AreNotEqual(smokeColumnName, op.ColumnName);
-		}
-
-		[Test]
-		public void ShouldSetPropertiesForRemoveIndexWithOptionalConstraintName()
-		{
-			const string schemaName = "schemaName";
-			const string tableName = "tableName";
-			var optionalConstraintName = "optionalConstraintName";
-
-			var op = new RemoveIndexOperation(schemaName, tableName, optionalConstraintName);
-			Assert.AreEqual(schemaName, op.SchemaName);
-			Assert.AreEqual(tableName, op.TableName);
-			Assert.That(op.ColumnName, Is.Null);
-			Assert.AreEqual(optionalConstraintName, op.Name.ToString());
-		}
-
-		[Test]
-		public void ShouldWriteQueryForRemoveIndex()
-		{
-			const string schemaName = "schemaName";
-			const string tableName = "tableName";
-			var columnName = new[] { "column1", "column2" };
-
-			var op = new RemoveIndexOperation(schemaName, tableName, columnName);
-			const string expectedQuery = "drop index [IX_schemaName_tableName_column1_column2] on [schemaName].[tableName]";
-			Assert.AreEqual(expectedQuery, op.ToQuery());
-		}
-
-		[Test]
-		public void ShouldWriteQueryForRemoveIndexWithOptionalConstraintName()
-		{
-			const string schemaName = "schemaName";
-			const string tableName = "tableName";
-			var optionalConstraintName = "optionalConstraintName";
-
-			var op = new RemoveIndexOperation(schemaName, tableName, optionalConstraintName);
-			const string expectedQuery = "drop index [optionalConstraintName] on [schemaName].[tableName]";
-			Assert.AreEqual(expectedQuery, op.ToQuery());
-		}
 
 	}
 }
