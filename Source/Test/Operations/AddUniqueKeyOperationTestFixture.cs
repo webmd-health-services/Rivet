d46fcd70206e937b27b7f0939cd5caebd8aafa55
Aaron Jensen <splatteredbits@users.noreply.github.com>
2013-12-20 13:20:46 -0800

    Moving add/remove operation tests into their own test fixtures.

--- Source/Test/Operations/AddUniqueKeyOperationTestFixture.cs
+++ Source/Test/Operations/AddUniqueKeyOperationTestFixture.cs
@@ -1,18 +1,11 @@
 ﻿using NUnit.Framework;
-using NUnit.Framework.Constraints;
 using Rivet.Operations;
 
 namespace Rivet.Test.Operations
 {
 	[TestFixture]
-	public sealed class AddRemoveUniqueKeyTestFixture
+	public sealed class AddUniqueKeyTestFixture
 	{
-		[SetUp]
-		public void SetUp()
-		{
-
-		}
-
 		[Test]
 		public void ShouldSetPropertiesForAddUniqueConstraint()
 		{
@@ -169,59 +162,6 @@ namespace Rivet.Test.Operations
 
 			var op = new AddUniqueKeyOperation(schemaName, tableName, columnName, clustered, fillfactor, options, fileGroup);
 			var expectedQuery = "alter table [schemaName].[tableName] add constraint [AK_schemaName_tableName_column1_column2] unique clustered ([column1], [column2]) with (option1, options2, fillfactor = 80) on fileGroup";
-			Assert.AreEqual(expectedQuery, op.ToQuery());
-		}
-
-		[Test]
-		public void ShouldSetPropertiesForRemoveUniqueConstraint()
-		{
-			var schemaName = "schemaName";
-			var tableName = "tableName";
-			string[] columnName = new string[] { "column1", "column2" };
-			string[] smokeColumnName = new string[] { "column1" };
-
-			var op = new RemoveUniqueKeyOperation(schemaName, tableName, columnName);
-			Assert.AreEqual(schemaName, op.SchemaName);
-			Assert.AreEqual(tableName, op.TableName);
-			Assert.AreEqual(columnName, op.ColumnName);
-			Assert.AreNotEqual(smokeColumnName, op.ColumnName);
-		}
-
-		[Test]
-		public void ShouldSetPropertiesForRemoveUniqueConstraintWithCustomName()
-		{
-			var schemaName = "schemaName";
-			var tableName = "tableName";
-			var customConstraintName = "customConstraintName";
-
-			var op = new RemoveUniqueKeyOperation(schemaName, tableName, customConstraintName);
-			Assert.AreEqual(schemaName, op.SchemaName);
-			Assert.AreEqual(tableName, op.TableName);
-			Assert.That(op.ColumnName, Is.Null);
-			Assert.AreEqual(customConstraintName, op.Name.ToString());
-		}
-
-		[Test]
-		public void ShouldWriteQueryForRemoveUniqueConstraint()
-		{
-			var schemaName = "schemaName";
-			var tableName = "tableName";
-			string[] columnName = new string[] { "column1", "column2" };
-
-			var op = new RemoveUniqueKeyOperation(schemaName, tableName, columnName);
-			var expectedQuery = "alter table [schemaName].[tableName] drop constraint [AK_schemaName_tableName_column1_column2]";
-			Assert.AreEqual(expectedQuery, op.ToQuery());
-		}
-
-		[Test]
-		public void ShouldWriteQueryForRemoveUniqueConstraintWithCustomName()
-		{
-			var schemaName = "schemaName";
-			var tableName = "tableName";
-			var customConstraintName = "customConstraintName";
-
-			var op = new RemoveUniqueKeyOperation(schemaName, tableName, customConstraintName);
-			var expectedQuery = "alter table [schemaName].[tableName] drop constraint [customConstraintName]";
 			Assert.AreEqual(expectedQuery, op.ToQuery());
 		}
 
