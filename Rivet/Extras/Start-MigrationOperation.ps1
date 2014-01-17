3923b4a675fb69567d3433b61e5654af4047c6dc
Aaron Jensen <splatteredbits@users.noreply.github.com>
2014-01-16 18:09:03 -0800

    Adding additional migration operation validations: identities, foreign keys, check constraints must all specify not for replication and no non-int identity columns.

--- Rivet/Extras/Start-MigrationOperation.ps1
+++ Rivet/Extras/Start-MigrationOperation.ps1
@@ -8,26 +8,65 @@ function Start-MigrationOperation
         $Operation
     )
 
-    if( -not ($Operation -is [Rivet.Operations.AddTableOperation]) )
+    $problems = $false
+    if( ($Operation -is [Rivet.Operations.AddTableOperation]) )
     {
-        return
+        Invoke-Command {
+            smalldatetime 'CreateDate' -NotNull -Default 'getdate()' -Description 'Record created date'
+            datetime 'LastUpdated' -NotNull -Default 'getdate()' -Description 'Date this record was last updated' 
+        } | ForEach-Object { $Operation.Columns.Add( $_ ) }
+
+        $skipRowGuidCol = $Operation.Columns | 
+                            Where-Object { $_.DataType -eq [Rivet.DataType]::UniqueIdentifier } |
+                            Where-Object { $_.RowGuidCol }
+        if( -not $skipRowGuidCol )
+        {
+            $Operation.Columns.Add( 
+                (uniqueidentifier 'rowguid' -NotNull -RowGuidCol -Default 'newsequentialid()' -Description 'rowguid column used for replication')
+            )
+
+        }
+
+        $Operation.Columns.Add( (bit 'SkipBit' -Default 0 -Description 'Used to bypass custom triggers') )
     }
 
-    Invoke-Command {
-        smalldatetime 'CreateDate' -NotNull -Default 'getdate()' -Description 'Record created date'
-        datetime 'LastUpdated' -NotNull -Default 'getdate()' -Description 'Date this record was last updated' 
-    } | ForEach-Object { $Operation.Columns.Add( $_ ) }
-
-    $skipRowGuidCol = $Operation.Columns | 
-                        Where-Object { $_.DataType -eq [Rivet.DataType]::UniqueIdentifier } |
-                        Where-Object { $_.RowGuidCol }
-    if( -not $skipRowGuidCol )
+    if( ($Operation -is [Rivet.Operations.AddTableOperation]) -or ($Operation -is [Rivet.Operations.UpdateTableOperation]) )
     {
-        $Operation.Columns.Add( 
-            (uniqueidentifier 'rowguid' -NotNull -RowGuidCol -Default 'newsequentialid()' -Description 'rowguid column used for replication')
-        )
+        Invoke-Command {
+            ('Columns','AddColumns','UpdateColumns') | 
+                Where-Object { $Operation | Get-Member $_ } |
+                ForEach-Object { $Operation | Select-Object -ExpandProperty $_ } |
+                ForEach-Object {
+                    if( $_.Identity )
+                    {
+                        if( $_.DataType -ne [Rivet.DataType]::Int )
+                        {
+                            Write-Error ('Table {0}: column {1}: {2} columns can''t be identity columns.  Please remove the identity specification or change the column type to int.' -f $Operation.Name,$_.Name,$_.DataType)
+                            $problems = $true
+                        }
 
+                        $_.Identity.NotForReplication = $true
+                    }
+                }
+        }
     }
 
-    $Operation.Columns.Add( (bit 'SkipBit' -Default 0 -Description 'Used to bypass custom triggers') )
+    if( $Operation -is [Rivet.Operations.AddForeignKeyOperation] -or $Operation -is [Rivet.Operations.AddCheckConstraintOperation] )
+    {
+        $Operation.NotForReplication = $true
+    }
+
+    if( $Operation -is [Rivet.Operations.AddTriggerOperation] -or $Operation -is [Rivet.Operations.UpdateTriggerOperation] )
+    {
+        if( $Operation.Definition -notmatch 'not for replication' )
+        {
+            Write-Error ('Trigger {0}: all user-defined triggers must have ''not for replication'' clause specified.  Please add the ''not for replication'' clause to your trigger.' -f $_.Name)
+            $problems = $true
+        }
+    }
+
+    if( $problems )
+    {
+        throw ('There were errors running ''{0}''.  Please see previous errors for details.' -f $Operation.GetType().Name)
+    }
 }
