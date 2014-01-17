f831683781f5ee6f2b99b22e0179222c6e2596e7
Aaron Jensen <ajensen@webmd.net>
2014-01-17 12:38:16 -0800

    Fixing broken test.

--- Test/RivetTest/Assert-DefaultConstraint.ps1
+++ Test/RivetTest/Assert-DefaultConstraint.ps1
@@ -7,6 +7,11 @@ function Assert-DefaultConstraint
     #>
 
     param(
+        [Parameter()]
+        [string]
+        # The table's schema.  Default is `dbo`.
+        $SchemaName = 'dbo',
+
         [Parameter(Mandatory=$true)]
         [string]
         # The name of the table
@@ -16,10 +21,9 @@ function Assert-DefaultConstraint
         # Array of Column Names
         $ColumnName,
 
-        [Parameter()]
         [string]
-        # The table's schema.  Default is `dbo`.
-        $SchemaName = 'dbo',
+        # The name of the constraint.
+        $Name,
 
         [string]
         # The expected expression.
@@ -28,10 +32,13 @@ function Assert-DefaultConstraint
 
     Set-StrictMode -Version Latest
 
-    $name = New-ConstraintName -ColumnName $ColumnName -TableName $TableName -SchemaName $SchemaName -Default
+    if( -not $Name )
+    {
+        $Name = New-ConstraintName -ColumnName $ColumnName -TableName $TableName -SchemaName $SchemaName -Default
+    }
 
-    $constraint = Get-DefaultConstraint -Name $name
-    Assert-NotNull $constraint ('Default constraint ''{0}'' not found.' -f $name)
+    $constraint = Get-DefaultConstraint -Name $Name
+    Assert-NotNull $constraint ('Default constraint ''{0}'' not found.' -f $Name)
 
     if( $PSBoundParameters.ContainsKey('Expression') )
     {
