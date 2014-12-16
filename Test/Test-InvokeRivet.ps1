5fcb73060dae6812ac38500e55c988f6305af5bb
Aaron Jensen <splatteredbits@users.noreply.github.com>
2014-12-16 05:39:00 -0800

    Adding support for multiple/different target database names from database name on file system.

--- Test/Test-InvokeRivet.ps1
+++ Test/Test-InvokeRivet.ps1
@@ -35,10 +35,56 @@ function Pop-Migration
 }
 '@ | New-Migration -Name 'CreateDatabase' -Database $name
 
-    $conn = Invoke-Rivet -Push -Database $name
-    Assert-NoError
-    Assert-Null $conn
+    try
+    {
+        $conn = Invoke-Rivet -Push -Database $name
+        Assert-NoError
+        Assert-Null $conn
 
-    $cmd = New-Object 'Data.SqlClient.SqlCommand' ($query,$RTMasterConnection)
-    Assert-True $cmd.ExecuteScalar()
+        $cmd = New-Object 'Data.SqlClient.SqlCommand' ($query,$RTMasterConnection)
+        Assert-True $cmd.ExecuteScalar()
+    }
+    finally
+    {
+        Remove-RivetTestDatabase -Name $name
+    }
+}
+
+function Test-ShouldApplyMigrationsToDuplicateDatabase
+{
+    $config = Get-Content -Raw -Path $RTConfigFilePath | ConvertFrom-Json
+    $config | Add-Member -MemberType NoteProperty -Name 'TargetDatabases' -Value @{ $RTDatabaseName = @( 'InvokeRivet', 'InvokeRivet2' ) }
+    $config | ConvertTo-Json | Set-Content -Path $RTConfigFilePath
+
+    @'
+function Push-Migration
+{
+    Add-Schema 'TargetDatabases'
+}
+
+function Pop-Migration
+{
+    Remove-Schema 'TargetDatabases'
+}
+'@ | New-Migration -Name 'TargetDatabases' -Database $RTDatabaseName
+
+    try
+    {
+        $result = Invoke-Rivet -Push -Database $RTDatabaseName
+        Assert-NoError
+        Assert-Null $result
+
+        Assert-False (Test-Schema -Name 'TargetDatabases')
+
+        $RTDatabaseConnection.ChangeDatabase( 'InvokeRivet' )
+        Assert-Schema -Name 'TargetDatabases'
+
+        $RTDatabaseConnection.ChangeDatabase( 'InvokeRivet2' )
+        Assert-Schema -Name 'TargetDatabases'
+    }
+    finally
+    {
+        Remove-RivetTestDatabase -Name 'InvokeRivet'
+        Remove-RivetTestDatabase -Name 'InvokeRivet2'
+    }
 }
