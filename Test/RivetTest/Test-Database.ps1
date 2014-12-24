7da6d0db6feb8cdf8e9676e963eb91a486a6f8cf
Aaron Jensen <ajensen@webmd.net>
2014-12-24 12:36:38 -0800

    Bug fix: Rivet doesn't migrate multiple target databases if the migrations directory is empty.

--- Test/RivetTest/Test-Database.ps1
+++ Test/RivetTest/Test-Database.ps1
@@ -2,11 +2,14 @@
 function Test-Database
 {
     param(
+        [string]
+        # The name of the database.
+        $Name = $RTDatabaseName
     )
 
     $query = @'
     select count(*) Count from sys.databases where Name = '{0}'
-'@ -f $RTDatabaseName
+'@ -f $Name
 
     $count = Invoke-RivetTestQuery -Query $query -Connection $RTMasterConnection -AsScalar
     return ($count -eq 1)
