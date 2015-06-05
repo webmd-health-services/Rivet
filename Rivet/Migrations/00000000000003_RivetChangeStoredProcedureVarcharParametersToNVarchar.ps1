a59eedfbe478cdbf5446c4b2952d08fd8bcdf2f9
Aaron Jensen <ajensen@webmd.net>
2015-06-05 16:50:14 -0700

    Removing whitespace from Rivet's internal migrations.

--- Rivet/Migrations/00000000000003_RivetChangeStoredProcedureVarcharParametersToNVarchar.ps1
+++ Rivet/Migrations/00000000000003_RivetChangeStoredProcedureVarcharParametersToNVarchar.ps1
@@ -2,29 +2,29 @@
 function Push-Migration
 {
     Update-StoredProcedure -SchemaName 'rivet' -Name 'InsertMigration' -Definition @'
-	        @ID bigint,
-	        @Name nvarchar(241),
-	        @Who nvarchar(50),
-	        @ComputerName nvarchar(50)
-        as
-        begin
-	        declare @AtUtc datetime2(7)
-	        select @AtUtc = getutcdate()
-	        insert into [rivet].[Migrations] ([ID],[Name],[Who],[ComputerName],[AtUtc]) values (@ID,@Name,@Who,@ComputerName,@AtUtc)
-	        insert into [rivet].[Activity] ([Operation],[MigrationID],[Name],[Who],[ComputerName],[AtUtc]) values ('Push',@ID,@Name,@Who,@ComputerName,@AtUtc)
-        end
+	@ID bigint,
+	@Name nvarchar(241),
+	@Who nvarchar(50),
+	@ComputerName nvarchar(50)
+as
+begin
+	declare @AtUtc datetime2(7)
+	select @AtUtc = getutcdate()
+	insert into [rivet].[Migrations] ([ID],[Name],[Who],[ComputerName],[AtUtc]) values (@ID,@Name,@Who,@ComputerName,@AtUtc)
+	insert into [rivet].[Activity] ([Operation],[MigrationID],[Name],[Who],[ComputerName],[AtUtc]) values ('Push',@ID,@Name,@Who,@ComputerName,@AtUtc)
+end
 '@
 
     Update-StoredProcedure -SchemaName 'rivet' -Name 'RemoveMigration' -Definition @'
-	        @ID bigint,
-            @Name nvarchar(241),
-            @Who nvarchar(50),
-            @ComputerName nvarchar(50)
-        as
-        begin
-	        delete from [rivet].[Migrations] where [ID] = @ID
-	        insert into [rivet].[Activity] ([Operation],[MigrationID],[Name],[Who],[ComputerName],[AtUtc]) values ('Pop',@ID,@Name,@Who,@ComputerName,getutcdate())
-        end
+	@ID bigint,
+    @Name nvarchar(241),
+    @Who nvarchar(50),
+    @ComputerName nvarchar(50)
+as
+begin
+	delete from [rivet].[Migrations] where [ID] = @ID
+	insert into [rivet].[Activity] ([Operation],[MigrationID],[Name],[Who],[ComputerName],[AtUtc]) values ('Pop',@ID,@Name,@Who,@ComputerName,getutcdate())
+end
 '@
 
 }
@@ -32,29 +32,29 @@ function Push-Migration
 function Pop-Migration
 {
    Update-StoredProcedure -SchemaName 'rivet' -Name 'InsertMigration' -Definition @'
-	        @ID bigint,
-	        @Name varchar(241),
-	        @Who varchar(50),
-	        @ComputerName varchar(50)
-        as
-        begin
-	        declare @AtUtc datetime2(7)
-	        select @AtUtc = getutcdate()
-	        insert into [rivet].[Migrations] ([ID],[Name],[Who],[ComputerName],[AtUtc]) values (@ID,@Name,@Who,@ComputerName,@AtUtc)
-	        insert into [rivet].[Activity] ([Operation],[MigrationID],[Name],[Who],[ComputerName],[AtUtc]) values ('Push',@ID,@Name,@Who,@ComputerName,@AtUtc)
-        end
+	@ID bigint,
+	@Name varchar(241),
+	@Who varchar(50),
+	@ComputerName varchar(50)
+as
+begin
+	declare @AtUtc datetime2(7)
+	select @AtUtc = getutcdate()
+	insert into [rivet].[Migrations] ([ID],[Name],[Who],[ComputerName],[AtUtc]) values (@ID,@Name,@Who,@ComputerName,@AtUtc)
+	insert into [rivet].[Activity] ([Operation],[MigrationID],[Name],[Who],[ComputerName],[AtUtc]) values ('Push',@ID,@Name,@Who,@ComputerName,@AtUtc)
+end
 '@
 
     Update-StoredProcedure -SchemaName 'rivet' -Name 'RemoveMigration' -Definition @'
-	        @ID bigint,
-            @Name varchar(241),
-            @Who varchar(50),
-            @ComputerName varchar(50)
-        as
-        begin
-	        delete from [rivet].[Migrations] where [ID] = @ID
-	        insert into [rivet].[Activity] ([Operation],[MigrationID],[Name],[Who],[ComputerName],[AtUtc]) values ('Pop',@ID,@Name,@Who,@ComputerName,getutcdate())
-        end
+	@ID bigint,
+    @Name varchar(241),
+    @Who varchar(50),
+    @ComputerName varchar(50)
+as
+begin
+	delete from [rivet].[Migrations] where [ID] = @ID
+	insert into [rivet].[Activity] ([Operation],[MigrationID],[Name],[Who],[ComputerName],[AtUtc]) values ('Pop',@ID,@Name,@Who,@ComputerName,getutcdate())
+end
 '@
 
 }
