b7a808c50261b6aa25e27cfc3845273637def937
Aaron Jensen <splatteredbits@users.noreply.github.com>
2015-08-25 18:00:59 -0700

    Bug fix: plugins run against already-pushed migrations.

---
+++ Test/RivetTest/New-PluginsRoot.ps1
@@ -0,0 +1,29 @@
+
+function New-PluginsRoot
+{
+    param(
+        [string]
+        $Prefix
+    )
+
+    Set-StrictMode -Version 'Latest'
+
+    $tempDir = New-TempDirectory -Prefix $Prefix
+
+    $rivetJson = Get-Content -Path $RTConfigFilePath | ConvertFrom-Json
+    if( -not $rivetJson )
+    {
+        return
+    }
+
+    if( -not ($rivetJson | Get-Member -Name 'PluginsRoot') )
+    {
+        $rivetJson | Add-Member -MemberType NoteProperty -Name 'PluginsRoot' -Value $tempDir.FullName
+    }
+
+    $rivetJson.PluginsRoot = $tempDir.FullName
+
+    $rivetJson | ConvertTo-Json -Depth 500 | Set-Content -Path $RTConfigFilePath
+
+    return $tempDir
+}
