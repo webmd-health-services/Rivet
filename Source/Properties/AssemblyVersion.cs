ad3958a743520a01e572ca94a15bda78b933814a
Aaron Jensen <ajensen@webmd.net>
2016-07-26 12:16:11 -0700

    Incrementing to version 0.8.0.

--- Source/Properties/AssemblyVersion.cs
+++ Source/Properties/AssemblyVersion.cs
@@ -22,8 +22,8 @@ using System.Reflection;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.7.0")]
+// [assembly: AssemblyVersion("0.8.0")]
 [assembly: AssemblyCopyright("Copyright 2013 - 2016 Aaron Jensen.")]
-[assembly: AssemblyVersion("0.7.0")]
-[assembly: AssemblyFileVersion("0.7.0")]
-[assembly: AssemblyInformationalVersion("0.7.0")]
+[assembly: AssemblyVersion("0.8.0")]
+[assembly: AssemblyFileVersion("0.8.0")]
+[assembly: AssemblyInformationalVersion("0.8.0")]
