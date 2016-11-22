bfcf01183b86ec24fdab74744515cd3178fd036a
Aaron Jensen <ajensen@webmd.net>
2016-11-22 12:39:12 -0800

    Combining build.ps1 and Test-Rivet.ps1; removing assemblies from repository.

--- build.ps1
+++ build.ps1
@@ -21,13 +21,8 @@ param(
     # The version to build. If not supplied, build the version as currently defined.
     $Version,
 
-    [string]
-    # The pre-release version, e.g. alpha.39, rc.1, etc.
-    $PreReleaseVersion,
-
-    [string]
-    # Build metadata.
-    $BuildMetadata
+    [Switch]
+    $Clean
 )
 
 #Requires -Version 4
@@ -35,12 +30,66 @@ Set-StrictMode -Version Latest
 
 & (Join-Path -Path $PSScriptRoot -ChildPath 'Silk\Import-Silk.ps1' -Resolve)
 
+.\init.ps1 
+
+$outputRoot = Join-Path -Path $PSScriptRoot -ChildPath 'Output'
+Install-Directory -Path $outputRoot
+
+if( $Clean )
+{
+    Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Source') -Include 'bin','obj' -Directory -Recurse | Remove-Item -Recurse
+    Get-ChildItem -Path $outputRoot | Remove-Item -Recurse -WhatIf
+}
+
 Set-ModuleVersion -ManifestPath (Join-Path -Path $PSScriptRoot -ChildPath 'Rivet\Rivet.psd1') `
                   -SolutionPath (Join-Path -Path $PSScriptRoot -ChildPath 'Source\Rivet.sln') `
                   -AssemblyInfoPath (Join-Path -Path $PSScriptRoot -ChildPath 'Source\Properties\AssemblyVersion.cs') `
                   -Version $Version `
-                  -PreReleaseVersion $PreReleaseVersion `
-                  -BuildMetadata $BuildMetadata `
                   -ReleaseNotesPath (Join-Path -Path $PSScriptRoot -ChildPath 'RELEASE_NOTES.txt' -Resolve) `
-                  -NuspecPath (Join-Path -Path $PSScriptRoot -ChildPath 'Rivet.nuspec' -Resolve)
+                  -NuspecPath (Join-Path -Path $PSScriptRoot -ChildPath 'Rivet.nuspec' -Resolve) `
+                  -Configuration 'Release'
 
+$failed = $false
+
+$xmlLogPath = $outputRoot
+$nunitLogPath = Join-Path -Path $xmlLogPath -ChildPath 'nunit.xml'
+
+$nunitPath = Join-Path -Path $PSScriptRoot -ChildPath 'packages\NUnit.ConsoleRunner\tools\nunit3-console.exe' -Resolve
+& $nunitPath (Join-Path -Path $PSScriptRoot -ChildPath 'Source\Test\bin\*\Rivet.Test.dll' -Resolve) "--result=$nunitLogPath;format=nunit2"
+if( $LASTEXITCODE -ne 0 )
+{
+    Write-Error -Message ('{0} NUnit tests failed. Check the build reports for more details.' -f $LASTEXITCODE)
+}
+
+robocopy /MIR (Join-Path -Path $PSScriptRoot -ChildPath 'Source\bin\Release') (Join-Path -Path $PSScriptRoot -ChildPath 'Rivet\bin') /NJH /NJS /NP /NDL
+
+$testRoot = Join-Path -Path $PSScriptRoot -ChildPath 'Test'
+
+# Let's get full stack traces in our errors.
+$bladeLogPath = Join-Path -Path $xmlLogPath -ChildPath 'blade.xml'
+& (Join-Path -Path $PSScriptRoot -ChildPath '.\Tools\Blade\blade.ps1' -Resolve) -Path $testRoot -XmlLogPath $bladeLogPath
+if( $LastBladeResult.Failures -or $LastBladeResult.Errors )
+{
+    Write-Error -Message ('{0} Blade tests failed, and {1} tests had errors. Check the build reports for more details.' -f $LastBladeResult.Failures,$LastBladeResult.Errors)
+    $failed = $true
+}
+
+$pesterLogPath = Join-Path -Path $xmlLogPath -ChildPath 'pester.xml'
+if( (Get-Module -Name 'Pester') )
+{
+    Remove-Module -Name 'Pester'
+}
+Import-Module (Join-Path -Path $PSScriptRoot -ChildPath 'Pester' -Resolve)
+$result = Invoke-Pester -Script $testRoot -OutputFile $pesterLogPath -OutputFormat LegacyNUnitXml -PassThru |
+                Select-Object -Last 1
+$result
+if( $result.FailedCount )
+{
+    Write-Error -Message ('{0} Pester tests failed. Check the NUnit reports for more details.' -f $result.FailedCount)
+    $failed = $true
+}
+
+if( $failed )
+{
+    exit 1
+}
