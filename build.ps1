262e289cd6f227350c2e7bf8c1f5a182bf7d87c9
Aaron Jensen <ajensen@webmd.net>
2018-11-20 14:05:08 -0800

    Add publishing API keys to Whiskey.

--- build.ps1
+++ build.ps1
@@ -31,4 +31,19 @@ if( $Initialize )
 }
 
 $context = New-WhiskeyContext -Environment 'Dev' -ConfigurationPath $configPath
+
+$apiKeys = @{
+    'PowerShellGallery' = 'POWERSHELL_GALLERY_API_KEY'
+    'github.com' = 'GITHUB_ACCESS_TOKEN'
+}
+
+$apiKeys.Keys |
+    Where-Object { Test-Path -Path ('env:{0}' -f $apiKeys[$_]) } |
+    ForEach-Object {
+        $apiKeyID = $_
+        $envVarName = $apiKeys[$apiKeyID]
+        Write-Verbose ('Adding API key "{0}" with value from environment variable "{1}".' -f $apiKeyID,$envVarName)
+        Add-WhiskeyApiKey -Context $context -ID $apiKeyID -Value (Get-Item -Path ('env:{0}' -f $envVarName)).Value
+    }
+
 Invoke-WhiskeyBuild -Context $context @optionalArgs
