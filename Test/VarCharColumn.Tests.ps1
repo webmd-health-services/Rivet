e34b34e308b14414d1a0214f803f5ff6d217e17b
Aaron Jensen <ajensen@webmd.net>
2019-05-08 10:05:26 -0700

    Migrating VarChar column tests to Pester (part 2).

--- Test/VarCharColumn.Tests.ps1
+++ Test/VarCharColumn.Tests.ps1
@@ -1,104 +1,101 @@
 
+Set-StrictMode -Version 'Latest'
 & (Join-Path -Path $PSScriptRoot -ChildPath 'RivetTest\Import-RivetTest.ps1' -Resolve)
 
-function Start-Test
-{
-    Start-RivetTest
-}
-
-function Stop-Test
-{
-    Stop-RivetTest
-}
-
-function Test-ShouldCreateVarCharColumn
-{
-    @'
-function Push-Migration
-{
-    Add-Table -Name 'Foobar' -Column {
-        VarChar 'id' -Max
-    } -Option 'data_compression = none'
-}
-
-function Pop-Migration
-{
-    Remove-Table 'Foobar'
-}
-
+Describe 'VarCharColumn' {
+    BeforeEach {
+        Start-RivetTest
+    }
+    
+    AfterEach {
+        Stop-RivetTest
+    }
+    
+    It 'should create var char column' {
+        @'
+    function Push-Migration
+    {
+        Add-Table -Name 'Foobar' -Column {
+            VarChar 'id' -Max
+        } -Option 'data_compression = none'
+    }
+    
+    function Pop-Migration
+    {
+        Remove-Table 'Foobar'
+    }
+    
 '@ | New-TestMigration -Name 'CreateVarCharColumn'
-
-    Invoke-RTRivet -Push 'CreateVarCharColumn'
     
-    Assert-Table 'Foobar'
-    Assert-Column -Name 'id' -DataType 'VarChar' -TableName 'Foobar' -Max
-}
-
-function Test-ShouldCreateVarCharColumnWithSparse
-{
-    @'
-function Push-Migration
-{
-    Add-Table -Name 'Foobar' -Column {
-        VarChar 'id' -Max -Sparse
+        Invoke-RTRivet -Push 'CreateVarCharColumn'
+        
+        Assert-Table 'Foobar'
+        Assert-Column -Name 'id' -DataType 'VarChar' -TableName 'Foobar' -Max
     }
-}
-
-function Pop-Migration
-{
-    Remove-Table 'Foobar'
-}
-
+    
+    It 'should create var char column with sparse' {
+        @'
+    function Push-Migration
+    {
+        Add-Table -Name 'Foobar' -Column {
+            VarChar 'id' -Max -Sparse
+        }
+    }
+    
+    function Pop-Migration
+    {
+        Remove-Table 'Foobar'
+    }
+    
 '@ | New-TestMigration -Name 'CreateVarCharColumnWithSparse'
-
-    Invoke-RTRivet -Push 'CreateVarCharColumnWithSparse'
     
-    Assert-Table 'Foobar'
-    Assert-Column -Name 'id' -DataType 'VarChar' -TableName 'Foobar' -Sparse -Max
-}
-
-function Test-ShouldCreateVarCharColumnWithNotNull
-{
-    @'
-function Push-Migration
-{
-    Add-Table -Name 'Foobar' -Column {
-        VarChar 'id' -Max -NotNull
+        Invoke-RTRivet -Push 'CreateVarCharColumnWithSparse'
+        
+        Assert-Table 'Foobar'
+        Assert-Column -Name 'id' -DataType 'VarChar' -TableName 'Foobar' -Sparse -Max
     }
-}
-
-function Pop-Migration
-{
-    Remove-Table 'Foobar'
-}
-
+    
+    It 'should create var char column with not null' {
+        @'
+    function Push-Migration
+    {
+        Add-Table -Name 'Foobar' -Column {
+            VarChar 'id' -Max -NotNull
+        }
+    }
+    
+    function Pop-Migration
+    {
+        Remove-Table 'Foobar'
+    }
+    
 '@ | New-TestMigration -Name 'CreateVarCharColumnWithNotNull'
-
-    Invoke-RTRivet -Push 'CreateVarCharColumnWithNotNull'
     
-    Assert-Table 'Foobar'
-    Assert-Column -Name 'id' -DataType 'VarChar' -TableName 'Foobar' -NotNull -Max
-}
-
-function Test-ShouldCreateVarCharColumnWithCustomSizeCollation
-{
-    @'
-function Push-Migration
-{
-    Add-Table -Name 'Foobar' -Column {
-        VarChar 'id' -NotNull -Size 50 -Collation "Chinese_Taiwan_Stroke_CI_AS"
+        Invoke-RTRivet -Push 'CreateVarCharColumnWithNotNull'
+        
+        Assert-Table 'Foobar'
+        Assert-Column -Name 'id' -DataType 'VarChar' -TableName 'Foobar' -NotNull -Max
     }
-}
-
-function Pop-Migration
-{
-    Remove-Table 'Foobar'
-}
-
-'@ | New-TestMigration -Name 'ShouldCreateVarCharColumnWithCustomSizeCollation'
-
-    Invoke-RTRivet -Push 'ShouldCreateVarCharColumnWithCustomSizeCollation'
     
-    Assert-Table 'Foobar'
-    Assert-Column -Name 'id' -DataType 'VarChar' -TableName 'Foobar' -NotNull -Size 50 -Collation "Chinese_Taiwan_Stroke_CI_AS"
+    It 'should create var char column with custom size collation' {
+        @'
+    function Push-Migration
+    {
+        Add-Table -Name 'Foobar' -Column {
+            VarChar 'id' -NotNull -Size 50 -Collation "Chinese_Taiwan_Stroke_CI_AS"
+        }
+    }
+    
+    function Pop-Migration
+    {
+        Remove-Table 'Foobar'
+    }
+    
+'@ | New-TestMigration -Name 'ShouldCreateVarCharColumnWithCustomSizeCollation'
+    
+        Invoke-RTRivet -Push 'ShouldCreateVarCharColumnWithCustomSizeCollation'
+        
+        Assert-Table 'Foobar'
+        Assert-Column -Name 'id' -DataType 'VarChar' -TableName 'Foobar' -NotNull -Size 50 -Collation "Chinese_Taiwan_Stroke_CI_AS"
+    }
 }
