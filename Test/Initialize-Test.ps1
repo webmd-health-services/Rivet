f0f5f051c07f73b573fb28de957ab7f9852658e6
Aaron Jensen <ajensen@webmd.net>
2019-05-31 10:54:34 -0700

    If RivetSamples is active during tests, things break.

--- Test/Initialize-Test.ps1
+++ Test/Initialize-Test.ps1
@@ -1,2 +1,10 @@
 
+#Requires -Version 5.1
+Set-StrictMode -Version 'Latest'
+
 & (Join-Path -Path $PSScriptRoot -ChildPath 'RivetTest\Import-RivetTest.ps1' -Resolve)
+
+if( (Get-Module -Name 'RivetSamples') )
+{
+    Remove-Module 'RivetSamples' -Force
+}
