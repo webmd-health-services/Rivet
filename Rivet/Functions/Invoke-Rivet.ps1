e5a1e5c63201380bb43d65b665b40b9cb859e815
Aaron Jensen <ajensen@webmd.net>
2020-05-01 11:11:05 -0700

    Allow users to load modules with their plug-ins instead of making Rivet do it.

--- Rivet/Functions/Invoke-Rivet.ps1
+++ Rivet/Functions/Invoke-Rivet.ps1
@@ -113,7 +113,7 @@ Found no databases to migrate. This can be a few things:
         return
     }
 
-    Import-RivetPlugin -Path $settings.PluginPaths
+    Import-RivetPlugin -Path $settings.PluginPaths -ModuleName $settings.PluginModules
 
     try
     {
