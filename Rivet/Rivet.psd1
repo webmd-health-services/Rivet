885a78ae7122a24a80d1e5c8a57b28d4d2fe49b9
Khoi Ky <kky@webmd.net>
2023-05-24 11:24:17 -0700

    Added an optional database parameter to the Connect-RivetSession function.

--- Rivet/Rivet.psd1
+++ Rivet/Rivet.psd1
@@ -11,7 +11,7 @@
     RootModule = 'Rivet.psm1'
 
     # Version number of this module.
-    ModuleVersion = '0.20.0'
+    ModuleVersion = '0.21.0'
 
     # ID used to uniquely identify this module
     GUID = '8af34b47-259b-4630-a945-75d38c33b94d'
---
+++ Test/Connect-RivetSession.Tests.ps1
@@ -0,0 +1,57 @@
+
+#Requires -Version 5.1
+Set-StrictMode -Version 'Latest'
+
+BeforeAll {
+    & (Join-Path -Path $PSScriptRoot -ChildPath 'Initialize-Test.ps1' -Resolve)
+
+    function ThenNoErrors
+    {
+        $Global:Error | Should -BeNullOrEmpty
+    }
+
+    function WhenConnectingRivet
+    {
+        param(
+            [String] $Database
+        )
+
+        $params = @{ }
+        if( $Database )
+        {
+            $params['Database'] = $Database
+        }
+
+        Connect-RivetSession -Session $RTSession @params
+    }
+}
+
+Describe 'Connect-RivetSession' {
+    AfterEach {
+        Stop-RivetTest
+    }
+
+    It 'should connect to specified database when given' {
+        Start-RivetTest -PhysicalDatabase @($RTDatabaseName, $RTDatabase2Name)
+        $RTSession.Connection.Database | Should -Be $RTDatabaseName
+        $RTSession.CurrentDatabase.Name | Should -Be $RTDatabaseName
+
+        WhenConnectingRivet -Database $RTDatabase2Name
+        $RTSession.Connection.Database | Should -Be $RTDatabase2Name
+        $RTSession.CurrentDatabase.Name | Should -Be $RTDatabase2Name
+
+        ThenNoErrors
+    }
+
+    It 'should connect to the default database when no databases are given' {
+        Start-RivetTest -PhysicalDatabase @($RTDatabaseName, $RTDatabase2Name)
+        $RTSession.Connection.Database | Should -Be $RTDatabaseName
+        $RTSession.CurrentDatabase.Name | Should -Be $RTDatabaseName
+
+        WhenConnectingRivet
+        $RTSession.Connection.Database | Should -Be $RTDatabaseName
+        $RTSession.CurrentDatabase.Name | Should -Be $RTDatabaseName
+
+        ThenNoErrors
+    }
+}
