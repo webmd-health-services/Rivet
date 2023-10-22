080beb8ab181002400fcf9672b728204d7ba245a
Khoi Ky <kky@webmd.net>
2023-10-22 11:19:24 -0700

    Remove warnings about missing constraints.

--- Test/RivetTest/Functions/Assert-DefaultConstraint.ps1
+++ Test/RivetTest/Functions/Assert-DefaultConstraint.ps1
@@ -32,7 +32,6 @@ function Assert-DefaultConstraint
 
     if( -not $Name )
     {
-        Write-Warning ('Constructing constraint names will soon become obsolete. Please update usages of Assert-DefaultConstraint/ThenDefaultConstraint to pass the name of the constraint instead of the schema name, table name, and column names.')
         $Name = New-RTConstraintName -ColumnName $ColumnName -TableName $TableName -SchemaName $SchemaName -Default
     }
 
